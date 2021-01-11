dfa8a7bf815f673bf439e1423693d234082ffcba
Chris Musson <chris.musson@hotmail.com>
2021-01-11 21:12:53 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Sudoku solver CBC
 
 ## Instructions 
-- Install python 3.7
-- Copy `cbcCInterfaceDll.dll` to `C:\Users\<Username>\AppData\Local\Programs\Python\Python37`
+- Install python 3.8
+- Copy `cbcCInterfaceDll.dll` to the same directory as you python executable, e.g. `C:\Users\<Username>\AppData\Local\Programs\Python\Python38`
 - Run in command prompt: `pip install mip`
 - To run the solver: `python sudoku.py <inputfile>`
--- code/sudokudata.py
+++ code/sudokudata.py
@@ -1,14 +1,13 @@
 class SudokuData:
-
     #reads sudoku.txt into a 3D boolean matrix
-    def __init__(self, fileName : str):
+    def __init__(self, filename : str):
         self.numbers = [[[0 for i in range(9)] for j in range(9)] for k in range(9)] 
 
-        f = open(fileName, 'r')
-        linenumber = 0
-        for l in f:
-            vls = l.split(' ')
-            for i in range(len(vls)):
-                if not '*' in vls[i]:
-                    self.numbers[linenumber][i][int(vls[i].rstrip())-1] = 1
-            linenumber += 1
+        f = open(filename, 'r')
+        line_num = 0
+        for line in f:
+            vals = line.strip().split(' ')
+            for i, val in enumerate(vals):
+                if val != "*":
+                    self.numbers[line_num][i][int(val.strip())-1] = 1
+            line_num += 1
